d46f597951a6dcf618baed88ba7966eff52f22e0
Murad255 <35770563+Murad255@users.noreply.github.com>
2021-04-24 11:38:20 +0300

    #VERSION-0.1
    добавление проекта BPMN системы

---
+++ HackInHomeBPMN/src/main/java/com/HackInHome/BPMN/Application.java
@@ -0,0 +1,13 @@
+package com.HackInHome.BPMN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
